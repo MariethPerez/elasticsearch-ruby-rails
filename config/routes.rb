00870f615b4db5a2b98b575226883356b98dad23
Marieth <mperezzevallos@gmail.com>
2020-06-29 15:38:27 -0500

    add routes and elasticsearch gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/edit'
+  # get 'posts/index'
+  # get 'posts/show'
+  # get 'posts/new'
+  # get 'posts/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'posts#index'
+
+  resources :posts
 end
